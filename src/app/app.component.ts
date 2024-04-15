8e834ea99e3ae6e22451c4bccb16e425f1620068
hanasl <hanaslouma8@gmail.com>
2024-04-15 15:04:55 +0100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ImageUploadComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
