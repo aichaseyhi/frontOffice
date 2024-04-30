4fdbc3f630d36200f795c69e03f41ea0b789a94f
Aicha Seyhi <aichaseyhi98@gmail.com>
2024-04-30 14:43:51 +0100

    cleint

--- src/app/secure/client/client.ts
+++ src/app/secure/client/client.ts
@@ -1,23 +1,14 @@
 export interface Client {
+
+    order_id?:string;
     id?:string;
     name?:string;
     email?:string;
     phone?:string;
     password?:string;
-    status?:string;
    // birthday?:Date;
     //sexe?:string;
-    image:ImageData;
-    role?:string;
     city?:string;
     street?:string;
     code_poste?:Int16Array;
-    acountLink?:string;
-    CIN?:Int16Array;
-    TAXNumber?:Int16Array;
-    companyName?:string;
-    companyUnderConstruction?:string;
-
-
-
 }
